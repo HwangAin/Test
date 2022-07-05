c0702275b04bd1e6f56811d5d21dce162ba91838
PARK GA IN <pgi082247@gmail.com>
2022-07-05 18:51:29 +0900

    test

---
+++ MERGE/.java
@@ -0,0 +1,33 @@
+public class ateamDTO {
+	private int salary;
+	private int days;
+	
+	
+	public ateamDTO(int salary, int days) {
+		this.salary = salary;
+		this.days = days;
+	}
+
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+	public int getDays() {
+		return days;
+	}
+
+
+	public void setDays(int days) {
+		this.days = days;
+	}
+	
+	
+	
+}
